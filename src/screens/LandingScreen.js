9a10832f455bdaea8472230f2162512e0a55fc91
shashankNT <shashank.k@navtech.io>
2024-01-17 17:39:22 +0530

    fix: the logout issue on reopening the app

--- src/screens/LandingScreen.js
+++ src/screens/LandingScreen.js
@@ -1,19 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SubmitButton from '../components/SubmitButton';
-import { ImageBackground, View, StyleSheet, Image } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { ImageBackground, View, StyleSheet, Image, ActivityIndicator } from 'react-native'
+import { appTheme } from '../colors';
 
 const LandingScreen = ({ navigation }) => {
+
+    const [loader, setLoader] = useState(true);
+
+    const checkForTocken = async () => {
+        const credentials = await AsyncStorage.getItem('basicAuth');
+        if (credentials !== null) navigation.navigate('Home');
+        else setLoader(false);
+    }
+
+    useEffect(() => {
+        checkForTocken();
+    }, [])
+
     return (
         <>
-            <View style={styles.container}>
-                <ImageBackground source={require('../images/introduction.png')} resizeMode="cover" style={styles.image}>
-                    <View style={{ alignItems: "center", marginHorizontal: 50, marginTop: 150, justifyContent: 'space-between', flex: 1 }}>
-                        <Image source={require("../images/logo_icon.jpg")} style={{ height: 200, aspectRatio: 1, paddingHorizontal: 20 }} />
-                        <SubmitButton text={'Get started'} onPress={() => navigation.navigate('LoginScreen')} />
-                    </View>
+            {loader
+                ?
+                <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+                    <ActivityIndicator size={70} color={appTheme.primaryColor} />
+                </View>
+                :
+                <View style={styles.container}>
+                    <ImageBackground source={require('../images/introduction.png')} resizeMode="cover" style={styles.image}>
+                        <View style={{ alignItems: "center", marginHorizontal: 50, marginTop: 150, justifyContent: 'space-between', flex: 1 }}>
+                            <Image source={require("../images/logo_icon.jpg")} style={{ height: 200, aspectRatio: 1, paddingHorizontal: 20 }} />
+                            <SubmitButton text={'Get started'} onPress={() => navigation.navigate('LoginScreen')} />
+                        </View>
 
-                </ImageBackground>
-            </View>
+                    </ImageBackground>
+                </View>
+            }
         </>
     )
 }
